c1157365ce891638946f7c2ff133d751956fe223
Odoo Migration Factory <sherpa-c2c@users.noreply.github.com>
2026-06-12 12:12:13 +0200

    [MIG] board_eval_context: Migration to 17.0

--- board_eval_context/__manifest__.py
+++ board_eval_context/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Board Eval Context",
     "summary": "Add some keys to board.board eval context",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainer": "gurneyalex",
     "depends": [
